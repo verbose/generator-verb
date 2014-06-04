f5b43bbb1d02841bb99125602b5de29d27f76ce5
jonschlinkert <dev@sellside.com>
2014-06-03 23:16:53 -0400

    adds `yo verb:tag` as a subgenerator. invokes generator-tag.

---
+++ tag/index.js
@@ -0,0 +1,26 @@
+/*!
+ * generator-verb <https://github.com/assemble/generator-verb>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+
+var VerbGenerator = module.exports = function VerbGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.hookFor('tag', {
+    args: args,
+    options: options,
+    config: config
+  });
+};
+
+util.inherits(VerbGenerator, yeoman.generators.Base);
+
+VerbGenerator.prototype.startHook = function startHook() {};
