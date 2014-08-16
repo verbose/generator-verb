b92c1b1e84d5fce4be6b8491615c6a7e0800be5b
jonschlinkert <dev@sellside.com>
2014-08-16 15:26:00 -0400

    v0.3.0

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -7,26 +7,36 @@
 
 'use strict';
 
+var fs = require('fs');
 var path = require('path');
 var util = require('util');
 var yeoman = require('yeoman-generator');
 var boilerplate = path.dirname(require.resolve('verb-boilerplates'));
 
 
-
 var VerbGenerator = module.exports = function VerbGenerator(args, options, config) {
-  // Make the 'node' boilerplate the default
+  // Use the 'node' boilerplate the default
   if (args.length === 0) {
     args[0] = 'node';
   }
   yeoman.generators.NamedBase.apply(this, arguments);
 };
+
 util.inherits(VerbGenerator, yeoman.generators.NamedBase);
 
 
 VerbGenerator.prototype.files = function files() {
-  if(this.name === 'docs') {
-    this.log.error('"docs" is not a valid boilerplate');
+  var dir = path.join(boilerplate, this.name);
+  var self = this;
+
+  var fn = {};
+  fn.shortname = require('app-name');
+  fn.namify = require('namify');
+  this._.mixin(fn);
+
+  if(!fs.existsSync(dir)) {
+    this.log.error('"' + dir + '" is not a valid boilerplate.');
   }
-  this.directory(path.join(boilerplate, this.name), 'docs');
+
+  this.directory(dir, process.cwd());
 };
